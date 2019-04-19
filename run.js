37bf24c9c28b90e998ca7892d97f792d3c4f7c6f
Christian Flach <cmfcmf.flach@gmail.com>
2019-04-19 02:05:09 +0200

    Enable keyboard events, switch to screencast api, use SmartBuffer, add second way to pass images

--- run.js
+++ run.js
@@ -1,73 +1,140 @@
 const puppeteer = require('puppeteer');
 const util = require("util");
+const SmartBuffer = require('smart-buffer').SmartBuffer;
+const getPixels = require("get-pixels")
 
 const wait = util.promisify(setTimeout);
 
+const IMAGE_FORMAT = "raw"; // "raw" or "png"
+
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    ignoreDefaultArgs: [
+      "--hide-scrollbars", // show scrollbars
+      "--mute-audio", // enable audio
+      // "--headless", // launch browser in normal window
+
+      // Other possible flags to remove-
+      // "--disable-renderer-backgrounding", "--disable-features", "--disable-dev-shm-usage", "--disable-background-networking"
+    ],
+    // I read somewhere that this makes it faster.
+    // args: ["--proxy-server='direct://'", '--proxy-bypass-list=*'],
+    executablePath: "/usr/bin/chromium-browser"
+  });
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
   await page.setViewport({width: 640, height: 480});
   await page.goto('https://google.com');
 
-  console.error('hello');
-  let currentStdinBuffer;
-  process.stdin.on('data', chunk => {
-    currentStdinBuffer = currentStdinBuffer ? Buffer.concat([currentStdinBuffer, chunk]) : chunk;
+  let currentStdinBuffer = new SmartBuffer();
+  process.stdin.on('data', async chunk => {
+    currentStdinBuffer.writeBuffer(chunk);
 
-    if (currentStdinBuffer.length > 0) {
-      const command = String.fromCharCode(currentStdinBuffer.readUInt8(0));
-      const size = currentStdinBuffer.readUInt32BE(1);
+    while (currentStdinBuffer.length) {
+      const command = String.fromCharCode(currentStdinBuffer.readUInt8());
+      const size = currentStdinBuffer.readUInt32BE();
       console.error('Received command', command, currentStdinBuffer.length, size);
       let x, y;
       switch (command) {
         case 'e':
           // TODO check read boundaries for each event type
-          const eventType = currentStdinBuffer.readUInt8(5);
+          const eventType = currentStdinBuffer.readUInt8();
           console.error('Received event type', eventType);
           switch (eventType) {
             case 0:
-              page.mouse.up();
+              page.mouse.up({ button: "left" });
               break;
             case 1:
-              page.mouse.down();
+              page.mouse.down({ button: "left" });
               break;
             case 2:
-              x = currentStdinBuffer.readUInt32LE(6);
-              y = currentStdinBuffer.readUInt32LE(10);
+              x = currentStdinBuffer.readUInt32LE();
+              y = currentStdinBuffer.readUInt32LE();
               console.error('Moving to', x, y);
               page.mouse.move(x, y);
               break;
             case 3:
-              // page.keyboard.down();
+              const squeakKeyString = currentStdinBuffer.readString(size, "ascii");
+              let keyName;
+              if (squeakKeyString.length > 1) {
+                const conversion = {
+                  '<space>': 'Space',
+		              '<tab>': 'Tab',
+		              '<cr>': 'Enter',
+		              '<lf>': 'Enter',
+		              '<enter>': 'Enter',
+		              '<backspace>': 'Backspace',
+		              '<delete>': 'Delete',
+		              '<escape>': 'Escape',
+		              '<down>': 'ArrowDown',
+		              '<up>': 'ArrowUp',
+		              '<left>': 'ArrowLeft',
+		              '<right>': 'ArrowRight',
+		              '<end>': 'End',
+		              '<home>': 'Home',
+		              '<pageDown>': 'PageDown',
+		              '<pageUp>': 'PageUp',
+		              // '<euro>': '', // TODO
+                  '<insert>': 'Insert',
+                };
+                keyName = conversion[squeakKeyString];
+              } else {
+                keyName = squeakKeyString;
+              }
+              console.error('Typing', squeakKeyString, keyName);
+
+              await page.keyboard.press(keyName);
               break;
             case 4:
-              // page.keyboard.up();
-              break;
-            case 5:
-              x = currentStdinBuffer.readUInt32LE(6);
-              y = currentStdinBuffer.readUInt32LE(10);
+              x = currentStdinBuffer.readUInt32LE();
+              y = currentStdinBuffer.readUInt32LE();
               page.setViewport({width: x, height: y});
               break;
           }
+          break;
       }
-      // TODO not very clean, should slice the buffer to the extent that we read
-      currentStdinBuffer = null;
+      // Construct a new buffer with the remaining content.
+      currentStdinBuffer = SmartBuffer.fromBuffer(currentStdinBuffer.readBuffer());
     }
   });
 
   process.on('SIGTERM', async () => {
+    await page.stopScreencast();
     await browser.close();
     process.exit();
   });
 
-  while(true) {
-    const screenshot = await page.screenshot({});
-    process.stdout.write("i");
-    const buf = new Buffer(4);
-    buf.writeUInt32BE(screenshot.length);
-    process.stdout.write(buf)
-    process.stdout.write(screenshot);
-    await wait(20);
-  }
+  page.on('screencastframe', async frame => {
+    const screenshot = Buffer.from(frame.data, 'base64');
 
+    if (IMAGE_FORMAT === "png") {
+      const buf = new SmartBuffer();
+      buf.writeString("ip");
+      buf.writeUInt32BE(screenshot.length);
+      process.stdout.write(buf.toBuffer());
+      process.stdout.write(screenshot);
+    } else if (IMAGE_FORMAT === "raw") {
+      const pixels = await new Promise((resolve, reject) => getPixels(screenshot, 'image/png', (err, pixels) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(pixels);
+        }
+      }));
+      const buf = new SmartBuffer();
+      buf.writeString("ir");
+      buf.writeUInt32BE(pixels.shape[0]);
+      buf.writeUInt32BE(pixels.shape[1]);
+      process.stdout.write(buf.toBuffer());
+      process.stdout.write(Buffer.from(pixels.data));
+    } else {
+      throw new Error(`Unsupported image format ${IMAGE_FORMAT}.`);
+    }
+
+    await page.screencastFrameAck(frame.sessionId);
+    console.error(Date.now() / 1000);
+  });
+
+  await page.startScreencast({format: 'png', everyNthFrame: 10});
+  console.error("Recording screencast...");
 })();
